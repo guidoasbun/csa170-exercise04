c128afdc45b43fe4b01ea2ad7f369ddb53819d19
guidoasbun <gdrodrigo8@gmail.com>
2020-09-15 22:22:37 -0700

    part A complete

--- Average.java
+++ Average.java
@@ -6,4 +6,20 @@
 *   Exercise 04
 */ 
 
-import 
+import java.util.Scanner;
+public class Average
+{
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter three numbers - I will average them: ");
+        
+        final int TOTAL_NUMBERS = 3;
+        double numberOne = input.nextInt();
+        double numberTwo = input.nextInt();
+        double numberThree = input.nextInt();
+        double average = (numberOne + numberTwo + numberThree) / TOTAL_NUMBERS;
+        
+        System.out.printf("The average of %.2f, %.2f, and %.2f is %.2f", numberOne, numberTwo, numberThree, average);
+    }
+}
